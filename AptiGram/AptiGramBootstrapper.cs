af1c9af48508d3e10659926fe257b48626c322ac
Håkan Alexander <hakan.alexander@gmail.com>
2015-11-20 13:02:56 +0100

    Error messages

---
+++ AptiGram/AptiGramBootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace AptiGram
+{
+    public class AptiGramBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ApplicationStartup(Nancy.TinyIoc.TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
+        {
+            StaticConfiguration.DisableErrorTraces = false;
+            base.ApplicationStartup(container, pipelines);
+        }
+    }
+}
