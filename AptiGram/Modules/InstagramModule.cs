0048f22d93e2fec08a73b3e4819500cbfc4d7c9e
Håkan Alexander <hakan.alexander@gmail.com>
2015-11-20 11:17:58 +0100

    Inital Commit

---
+++ AptiGram/Modules/InstagramModule.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using InstagramCSharp.Enums;
+using InstagramCSharp.Models;
+using InstagramCSharp.OAuth;
+using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using System.Configuration;
+
+namespace AptiGram.Modules
+{
+    public class InstagramModule : NancyModule
+    {
+        public InstagramModule()
+        {
+            Get["/", true] = async (x, ct) =>
+                {
+                    using (var client = new WebClient())
+                    {
+                        return client.DownloadString(new Uri("https://api.instagram.com/v1/users/2261366739/media/recent?access_token=" + ConfigurationManager.AppSettings["access_token"]));
+                    }
+                };
+        }
+    }
+}
