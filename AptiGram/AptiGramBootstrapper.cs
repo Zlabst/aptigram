51b7f9bd217e3b35bdd092f1759adad488f751b9
Håkan Alexander <hakan.alexander@gmail.com>
2015-11-20 13:21:31 +0100

    Enabled CORS

--- AptiGram/AptiGramBootstrapper.cs
+++ AptiGram/AptiGramBootstrapper.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Nancy;
+﻿using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
 
 namespace AptiGram
 {
@@ -12,6 +10,13 @@ namespace AptiGram
         {
             StaticConfiguration.DisableErrorTraces = false;
             base.ApplicationStartup(container, pipelines);
+        }
+
+        protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
+        {
+            pipelines.AfterRequest.AddItemToEndOfPipeline((ctx) => ctx.Response.WithHeader("Access-Control-Allow-Origin", "*")
+                                                                      .WithHeader("Access-Control-Allow-Methods", "GET")
+                                                                      .WithHeader("Access-Control-Allow-Headers", "Accept, Origin, Content-type"));
         }
     }
 }
